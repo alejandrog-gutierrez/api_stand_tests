db1990c0c45df60d2e3140b32bd5bc3110591fd6
Alejandro Gutiérrez <aggdl2428@gmail.com>
2026-02-23 15:11:59 -0500

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,7 @@
+
+URL_SERVICE = 'https://cnt-e13bdb62-a77e-4a2c-8787-efa2b438a25b.containerhub.tripleten-services.com/'
+DOC_PATH = '/docs/'
+LOG_MAIN_PATH = "/api/logs/main/"
+LOG_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
